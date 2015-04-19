502028b6557602ab67cd5ee28d40e9ce93aab5e0
Nirmesh <nbkhande@ncsu.edu>
2015-04-19 01:50:21 -0400

    Updaint Install script

--- setup.sh
+++ setup.sh
@@ -19,4 +19,21 @@ sudo apt-get install git-core -y
 sudo pip install virtualenv
 sudo apt-get install python-dev -y
 sudo pip install streamparse
-sudo apt-get install redis-server
+
+# Install MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo service mongod start
+
+#Install Redis
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable
+make
+cd src
+sudo make install
+
+sudo service mongod start
+mongoimport --db twitterstream --collection cwctweets --file data/cwctweets.json
