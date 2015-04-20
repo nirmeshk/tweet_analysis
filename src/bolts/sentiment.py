2c89484eed09c92dc7cf599f49a922884901f9bb
Nisarg <nisarg.gandhi64@gmail.com>
2015-04-19 22:58:13 -0400

    Sentiment bolt modified

--- src/bolts/sentiment.py
+++ src/bolts/sentiment.py
@@ -2,19 +2,21 @@ from __future__ import absolute_import, print_function, unicode_literals, divisi
 
 from streamparse.bolt import Bolt
 import redis
+from helper.sentiment-analysis import SentimentAnalysis
 
 class Sentiment(Bolt):
     """ This Bolt will calculate sentiment of tweet text in particular time slot and store it in hash data structure in Redis"""
 
     def initialize(self, conf, ctx):
         pass
+	self.s = SentimentAnalysis()
 
     def process(self, tup):
         slot = tup.values[0]
         txt = tup.values[1]
 
         #calculate sentiment of txt
-        sentiment = "pos"
+        sentiment = self.s.get_sentiment(txt)
         
         # prepare hash to store in redis
         redis_hash = "time_slot:" + str(slot)
