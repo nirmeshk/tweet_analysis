441ad8cc59f3be2bfe8c0d1d2342aacae0264822
Nisarg <nisarg.gandhi64@gmail.com>
2015-04-23 22:34:37 -0400

    sentiment and config file added

--- src/bolts/time_slot_creation.py
+++ src/bolts/time_slot_creation.py
@@ -2,19 +2,25 @@ from __future__ import absolute_import, print_function, unicode_literals, divisi
 
 from streamparse.bolt import Bolt
 import math
+import helper.readproperties
 
 class TimeSlotCreation(Bolt):
 
     def initialize(self, conf, ctx):
+        config = ReadProperties()
+        prop = config.getProperties
         # size of slot duration
-        self.duration = 5000
+        #self.duration = 5000
+        self.duration = prop["slot_duration"]
         # start time of slot
-        self.start_ts = 1427329782738
+        #self.start_ts = 1427329782738
+        self.start_ts = prop["ts_start"]
 
     def process(self, tup):
         tweet = tup.values[0]
         # get time stamp value
         ts = int(tweet['ts'])
+        self.log( "Duration: %s" % self.duration)
         slot = getTimeSlot(ts , self.start_ts, self.duration)
         txt = tweet['txt']
         self.emit( [slot, txt] )
