43619af1a45df3029f5a3bd0b89202a3cf7be159
Nisarg <nisarg.gandhi64@gmail.com>
2015-04-24 23:23:34 -0400

    start time, end time and slot no added to the time slot hash

--- src/bolts/time_slot_creation.py
+++ src/bolts/time_slot_creation.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals, division
 
 from streamparse.bolt import Bolt
+import redis
 import math
 #import helper.readproperties
 
@@ -15,6 +16,7 @@ class TimeSlotCreation(Bolt):
         # start time of slot
         self.start_ts = 1427329782738
         #self.start_ts = prop["ts_start"]
+        self.r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
     def process(self, tup):
         tweet = tup.values[0]
@@ -23,6 +25,18 @@ class TimeSlotCreation(Bolt):
         self.log( "Duration: %s" % self.duration)
         slot = getTimeSlot(ts , self.start_ts, self.duration)
         txt = tweet['txt']
+        # prepare hash to store in redis
+        redis_hash = "time_slot:" + str(slot)
+
+        # Calculating start and time time of the slot
+        end_time = self.start_ts + self.duration * slot
+        start_time = self.start_ts + self.duration * (slot - 1)
+
+        # Initialize slot number, start time, end time field of hash
+        self.r.hset(redis_hash, "slot_no", slot)
+        self.r.hset(redis_hash, "end_time", end_time)
+        self.r.hset(redis_hash, "start_time", start_time)
+
         self.emit( [slot, txt] )
         self.log( "%s: %s" % (slot, txt) )
 
