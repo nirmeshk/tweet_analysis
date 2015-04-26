37745eb4a122837d71844e790f2a25efe792fd49
Nirmesh <nbkhande@ncsu.edu>
2015-04-26 14:47:13 -0400

    updating istallation script

--- setup.sh
+++ setup.sh
@@ -1,31 +1,28 @@
-sudo apt-get update 
-sudo echo Y | apt-get install python-pip
-
+apt-get update 
+apt-get update 
+echo Y | apt-get install python-pip
+apt-get install wget -y
 # Installing Java
-sudo echo Y | apt-get install python-software-properties
-sudo echo Y | apt-get install software-properties-common
-sudo add-apt-repository ppa:webupd8team/java
-sudo apt-get update
-sudo echo Y | apt-get install oracle-java8-installer -y
+apt-get install openjdk-7-jdk -y
 
 #Installing leign
-sudo wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
-sudo mv lein /bin/
-sudo chmod a+x /bin/lein
-sudo lein -version
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+mv lein /bin/
+chmod a+x /bin/lein
+lein -version
 
 # Install virtual-env, git 
-sudo apt-get install git-core -y
-sudo pip install virtualenv
-sudo apt-get install python-dev -y
-sudo pip install streamparse
+apt-get install git-core -y
+pip install virtualenv
+apt-get install python-dev -y
+pip install streamparse
 
 # Install MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
+apt-get update
+apt-get install -y mongodb-org
+service mongod start
 
 #Install Redis
 wget http://download.redis.io/redis-stable.tar.gz
@@ -33,7 +30,13 @@ tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
 cd src
-sudo make install
+make install
+cd ~
 
-sudo service mongod start
-mongoimport --db twitterstream --collection cwctweets --file data/cwctweets.json
+service mongod start
+
+#Install all python dependencies
+#cd ~/tweet_analysis/
+#pip install -r requirements.txt
+#cd ~
+#mongoimport --db twitterstream --collection cwctweets --file tweet_analysis/data/cwctweets.json
