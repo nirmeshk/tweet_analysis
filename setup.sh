2f8d431a2f39029f1ef0cbbae30237991bba637c
Nirmesh <nbkhande@ncsu.edu>
2015-05-01 21:38:32 -0400

    updating installation script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 apt-get update > installation.log 
 apt-get install build-essential -y > installation.log
 
-echo "Installing Pip, wget .."
+echo "Installing Pip, wget..."
 apt-get install python-pip -y > installation.log
 apt-get install wget -y > installation.log
 # Installing Java
@@ -13,9 +13,8 @@ echo "Installing Leiningen..."
 wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > installation.log
 mv lein /bin/
 chmod a+x /bin/lein
-env LEIN_ROOT=yes
-yes | lein -version
-lein
+export LEIN_ROOT="yes"
+echo yes | lein -version > installation.log
 
 apt-get install python-dev -y > installation.log
 
@@ -23,7 +22,7 @@ echo "Installing streamparse library..."
 pip install streamparse > installation.log
 
 # Install MongoDB
-echo "Installing MongoDB.."
+echo "Installing MongoDB..."
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > installation.log
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list > installation.log
 apt-get update > installation.log
@@ -37,14 +36,14 @@ cd redis-stable
 make > installation.log
 cd src
 make install > installation.log
-redis-server &
+redis-server & > installation.log
 cd ~
 sleep 1
 
-service mongod start &
+service mongod start & > installation.log
 sleep 1
 
-echo "Install all python dependencies for project.."
+echo "Install all python dependencies for project..."
 cd ~/tweet_analysis/
 pip install -r requirements.txt > installation.log
 
